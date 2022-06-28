a495ad9f6e54f4aa5b2e819260a5ce0a45570879
anndimi <dimitrakopoulos.anna@gmail.com>
2022-06-28 14:07:45 +0200

    started with functionality to get single character

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
-// import express from 'express'
-// import cors from 'cors'
-// import dotenv from 'dotenv'
 const express = require('express')
 const cors = require('cors')
-const dotenv = require('dotenv')
+require('dotenv').config()
 const { MongoClient } = require('mongodb')
 
 const port = process.env.PORT
 const app = express()
 
 app.use(express.json())
-dotenv.config()
+
 app.use(cors())
 
 async function main() {
-  const connectionUri = process.env.MONGO_URL
+  const connectionUri = process.env.MONGO_URI
   const client = new MongoClient(connectionUri)
   await client.connect()
   const databases = await client.db().admin().listDatabases()
   console.log(databases)
 }
 
-//Function to fetch characters from db
+//Function to fetch all characters from db
 async function fetchWhitehackCharacters() {
-  const connectionUri = process.env.MONGO_URL
+  const connectionUri = process.env.MONGO_URI
   const client = new MongoClient(connectionUri)
   await client.connect()
 
@@ -41,13 +38,40 @@ async function fetchWhitehackCharacters() {
   return charactersArray
 }
 
-//Characters endpoint
+//Function to fetch single character from debugger
+async function fetchSingleWhitehackCharacter(id) {
+  const connectionUri = process.env.MONGO_URI
+  const client = new MongoClient(connectionUri)
+  await client.connect()
+
+  const query = { _id: id }
+
+  const findSingleWhitehackCharacter = await client
+    .db('characters-tracker')
+    .collection('characters')
+    .find(query)
+
+  console.log(findSingleWhitehackCharacter)
+
+  return findSingleWhitehackCharacter
+}
+
+//All characters endpoint
 app.get('/characters', async (req, res) => {
   const whitehackCharacters = await fetchWhitehackCharacters()
 
   console.log(whitehackCharacters)
 
   res.send(JSON.stringify(whitehackCharacters))
+})
+
+//Single character endpoint
+app.get('/characters/character/:name', async (req, res) => {
+  const characterInfo = await fetchSingleWhitehackCharacter(req.params.id)
+
+  res.send(JSON.stringify(characterInfo))
+
+  return characterInfo
 })
 
 app.listen(port, () => {
