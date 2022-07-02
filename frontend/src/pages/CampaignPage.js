18585ba08ed35eff27e11b35c96adf2bfaf33c81
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2022-07-02 12:22:59 +0200

    campaign component: added button for new story entry, added placeholder date and signature

--- frontend/src/pages/CampaignPage.js
+++ frontend/src/pages/CampaignPage.js
@@ -4,6 +4,8 @@ import Navbar from '../elements/Navbar'
 import styled from 'styled-components'
 import divider from '../assets/images/divider.png'
 import { Divider } from '../styles/global'
+import quilIcon from '../assets/icons/quil-icon.png'
+import sealImg from '../assets/images/seal-img.png'
 
 const CampaignPage = () => {
   let location = useLocation()
@@ -13,8 +15,13 @@ const CampaignPage = () => {
       {location.pathname === '/campaign' ? (
         <PageContainer>
           <h1>Adventures Under a Dying Sun</h1>
+          <AddEntryButton>
+            <img src={quilIcon} />
+          </AddEntryButton>
+          New Entry
           <CampaignWrapper>
             <Divider src={divider} alt="divider" />
+            <h4>Saturday, 02/07</h4>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
@@ -30,6 +37,10 @@ const CampaignPage = () => {
               pellentesque massa placerat duis ultricies lacus. Eget egestas
               purus viverra accumsan in nisl nisi scelerisque.
             </p>
+            <EntrySignature>
+              <SealImg src={sealImg} />
+              <span>Idris Kushul</span>
+            </EntrySignature>
             <Divider src={divider} alt="divider" />
           </CampaignWrapper>
         </PageContainer>
@@ -61,5 +72,48 @@ const CampaignWrapper = styled.div`
   padding: 30px 0;
   p {
     font-size: 20px;
+    width: 80%;
   }
+  h4 {
+    margin: 0;
+    width: 80%;
+    font-size: 30px;
+    font-family: 'Splash', cursive;
+    font-weight: normal;
+  }
+`
+
+const AddEntryButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+  margin-top: 30px;
+  img {
+    :hover {
+      transform: scale(1.09);
+    }
+    :active {
+      tranform: scale(1);
+    }
+  }
+`
+
+const EntrySignature = styled.div`
+  display: flex;
+  align-self: flex-end;
+  align-items: flex-end;
+  width: 90%;
+  span {
+    font-family: 'Splash', cursive;
+    font-size: 30px;
+    line-height: 100px;
+  }
+`
+
+const SealImg = styled.img`
+  width: 100%;
+  max-width: 100px;
 `
