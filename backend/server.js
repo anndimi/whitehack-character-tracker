fa7903b15277a8a73c9620de0597fbc3a275ef41
anndimi <dimitrakopoulos.anna@gmail.com>
2022-07-19 10:28:38 +0200

    added netlify url to corsOptions

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin:
+    'http://localhost:3000' || 'https://whitehackcharactertracker.netlify.app',
   optionsSuccessStatus: 200,
 }
 
