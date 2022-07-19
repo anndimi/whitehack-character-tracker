c8f9acf12f2781a4809424159b2ccf4c1e406ad3
anndimi <dimitrakopoulos.anna@gmail.com>
2022-07-19 10:34:39 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,10 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-  origin:
-    'http://localhost:3000' || 'https://whitehackcharactertracker.netlify.app/',
+  origin: [
+    'http://localhost:3000',
+    'https://whitehackcharactertracker.netlify.app/',
+  ],
   optionsSuccessStatus: 200,
 }
 
