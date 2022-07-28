44a0a2f84d658ea7668afad54e41cc9d5d1030d9
anndimi <dimitrakopoulos.anna@gmail.com>
2022-07-28 11:39:59 +0200

    moved modal styling to modal.js, cleaned up modal components

--- frontend/src/elements/EditCharacterModal.js
+++ frontend/src/elements/EditCharacterModal.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import styled from 'styled-components'
 import crossIcon from '../assets/icons/cross-icon.png'
 import closeIcon from '../assets/icons/close-icon.png'
+import {
+  ClassWrapper,
+  ClassContainer,
+  ScoreGroupsContainer,
+  AttributesContainer,
+  ModalContainer,
+  ModalWrapper,
+  ModalHeader,
+  CloseButton,
+  ModalBody,
+  Form,
+  ModalButtonWrapper,
+} from '../styles/modal'
 
 const EditCharacterModal = (props) => {
   const { id } = useParams()
@@ -310,7 +322,7 @@ const EditCharacterModal = (props) => {
 
             <ModalButtonWrapper>
               <button type="submit">
-                <img src={crossIcon} alt="add hero" />
+                <img src={crossIcon} alt="submit hero changes" />
               </button>
             </ModalButtonWrapper>
           </Form>
@@ -321,199 +333,3 @@ const EditCharacterModal = (props) => {
 }
 
 export default EditCharacterModal
-
-const ClassWrapper = styled.div`
-  fieldset {
-    border: none;
-    margin: 0;
-    padding: 0;
-  }
-`
-
-const ClassContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  padding: 20px 5px;
-  label {
-    display: inline-flex;
-    align-items: center;
-  }
-  input[type='radio'] {
-    position: relative;
-    height: 25px;
-    width: 25px;
-    opacity: 0;
-    cursor: pointer;
-    &:checked ~ span:after {
-      display: block;
-    }
-  }
-  span {
-    position: relative;
-    height: 25px;
-    width: 25px;
-    left: -20%;
-    cursor: pointer;
-    background-color: rgb(221, 208, 193);
-    border: 3px double #393939;
-    display: inline-block;
-    :after {
-      content: 'x';
-      position: relative;
-      font-family: 'Splash', cursive;
-      color: #393939;
-      font-size: 70px;
-      line-height: 5px;
-      display: none;
-    }
-  }
-`
-
-const ScoreGroupsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const AttributesContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  label {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  input {
-    width: 25% !important;
-  }
-`
-
-const ModalContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #05030399;
-  z-index: 9999;
-`
-
-const ModalWrapper = styled.div`
-  width: 500px;
-  height: 90%;
-  overflow-y: scroll;
-  background-color: rgb(221, 208, 193);
-  border-radius: 2px;
-  z-index: 9999;
-`
-
-const ModalHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 20px;
-  h3 {
-    font-size: 24px;
-    font-family: 'MedievalSharp', cursive;
-    padding-left: 40px;
-  }
-`
-
-const CloseButton = styled.button`
-  width: 50px !important;
-  margin: 0 !important;
-  background: transparent;
-  border: 1px solid transparent;
-  cursor: pointer;
-  img {
-    width: 30px;
-    vertical-align: middle;
-    :hover {
-      transform: scale(1.09);
-    }
-    :active {
-      transform: scale(1);
-    }
-  }
-`
-
-const ModalBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 5px 15px 0 15px;
-`
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  width: 80%;
-  margin: auto;
-  gap: 12px;
-  font-size: 18px;
-  input {
-    width: 100%;
-    height: 28px;
-    font-size: 20px;
-    border: 1px solid rgb(221, 208, 193);
-    border-bottom: 1px dotted #393939;
-    background-color: transparent;
-    font-family: inherit;
-    color: #393939;
-    :focus {
-      border: 1px solid rgb(221, 208, 193);
-      outline-offset: 0px !important;
-      outline: none !important;
-      border-bottom: 1px dotted #393939;
-    }
-    &:-webkit-autofill,
-    &:-webkit-autofill:hover,
-    &:-webkit-autofill:focus,
-    &:-webkit-autofill:active {
-      -webkit-box-shadow: 0 0 0 30px rgb(221, 208, 193) inset !important;
-    }
-    &::-webkit-outer-spin-button,
-    &::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
-  }
-  textarea {
-    resize: none;
-    width: 100%;
-    height: 300px;
-    border: 3px double #393939;
-    font-family: inherit;
-    color: #393939;
-    font-size: 18px;
-    background-color: transparent;
-    :focus {
-      border: 3px double #393939;
-      outline-offset: 0px !important;
-      outline: none !important;
-    }
-  }
-`
-
-const ModalButtonWrapper = styled.div`
-  text-align: center;
-  width: 100%;
-  align-self: center;
-  padding: 20px 15px 20px 15px;
-  margin-top: 10px;
-  button {
-    cursor: pointer;
-    background: transparent;
-    border: 1px solid transparent;
-  }
-
-  img {
-    :hover {
-      transform: scale(1.09);
-    }
-    :active {
-      transform: scale(1);
-    }
-  }
-`
---
+++ frontend/src/styles/modal.js
@@ -0,0 +1,194 @@
+import styled from 'styled-components'
+
+export const ClassWrapper = styled.div`
+  fieldset {
+    border: none;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export const ClassContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 20px 5px;
+  label {
+    display: inline-flex;
+    align-items: center;
+  }
+  input[type='radio'] {
+    position: relative;
+    height: 25px;
+    width: 25px;
+    opacity: 0;
+    cursor: pointer;
+    &:checked ~ span:after {
+      display: block;
+    }
+  }
+  span {
+    position: relative;
+    height: 25px;
+    width: 25px;
+    left: -20%;
+    cursor: pointer;
+    background-color: rgb(221, 208, 193);
+    border: 3px double #393939;
+    display: inline-block;
+    :after {
+      content: 'x';
+      position: relative;
+      font-family: 'Splash', cursive;
+      color: #393939;
+      font-size: 70px;
+      line-height: 5px;
+      display: none;
+    }
+  }
+`
+
+export const ScoreGroupsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const AttributesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  label {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  input {
+    width: 25% !important;
+  }
+`
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #05030399;
+  z-index: 9999;
+`
+
+export const ModalWrapper = styled.div`
+  width: 500px;
+  height: 90%;
+  background-color: rgb(221, 208, 193);
+  border-radius: 2px;
+  z-index: 9999;
+  overflow-y: scroll;
+`
+
+export const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
+  h3 {
+    font-size: 24px;
+    font-family: 'MedievalSharp', cursive;
+    padding-left: 40px;
+  }
+`
+
+export const CloseButton = styled.button`
+  width: 50px !important;
+  margin: 0 !important;
+  background: transparent;
+  border: 1px solid transparent;
+  img {
+    width: 30px;
+    vertical-align: middle;
+    :hover {
+      transform: scale(1.09);
+    }
+    :active {
+      transform: scale(1);
+    }
+  }
+`
+
+export const ModalBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 5px 15px 0 15px;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin: auto;
+  gap: 12px;
+  font-size: 18px;
+  input {
+    width: 100%;
+    height: 28px;
+    font-size: 20px;
+    border: 1px solid rgb(221, 208, 193);
+    border-bottom: 1px dotted #393939;
+    background-color: transparent;
+    font-family: inherit;
+    color: #393939;
+    :focus {
+      border: 1px solid rgb(221, 208, 193);
+      outline-offset: 0px !important;
+      outline: none !important;
+      border-bottom: 1px dotted #393939;
+    }
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      -webkit-box-shadow: 0 0 0 30px rgb(221, 208, 193) inset !important;
+    }
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  }
+  textarea {
+    resize: none;
+    width: 100%;
+    height: 300px;
+    border: 3px double #393939;
+    font-family: inherit;
+    color: #393939;
+    font-size: 18px;
+    background-color: transparent;
+    :focus {
+      border: 3px double #393939;
+      outline-offset: 0px !important;
+      outline: none !important;
+    }
+  }
+`
+
+export const ModalButtonWrapper = styled.div`
+  text-align: center;
+  width: 100%;
+  align-self: center;
+  padding: 20px 15px 20px 15px;
+  button {
+    background: transparent;
+    border: 1px solid transparent;
+  }
+
+  img {
+    :hover {
+      transform: scale(1.09);
+    }
+    :active {
+      transform: scale(1);
+    }
+  }
+`
